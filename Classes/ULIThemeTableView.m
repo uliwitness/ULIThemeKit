fefa496780ae27a61ae5ba3952bf72783209ae87
Uli Kusterer <ulivc@zathras.de>
2012-10-22 01:08:29 +0200

    Draw table rows separately so we can have alternating row colors.

--- Classes/ULIThemeTableView.m
+++ Classes/ULIThemeTableView.m
@@ -12,15 +12,24 @@
 
 -(void)	drawRow: (NSInteger)inRow clipRect: (NSRect)inClipRect
 {
-	NSRect		box = [self rectOfRow: inRow];
-	if( [self.selectedRowIndexes containsIndex: inRow] )
-		[NSColor.greenColor set];
-	else
-		[NSColor.blueColor set];
-	[NSBezierPath fillRect: box];
-	
 	if( inRow >= 0 && inRow <= self.numberOfRows )
 	{
+		NSArray	*	altRowColors = self.alternatingRowColors;
+		NSRect		box = [self rectOfRow: inRow];
+		if( [self.selectedRowIndexes containsIndex: inRow] )
+		{
+			if( self.window.isKeyWindow || self.window.isMainWindow )
+				[NSColor.greenColor set];
+			else
+				[NSColor.lightGrayColor set];
+		}
+		else
+		{
+			NSColor	*	currColor = [altRowColors objectAtIndex: inRow % altRowColors.count];
+			[currColor set];
+		}
+		[NSBezierPath fillRect: box];
+		
 		for( NSInteger x = 0; x < self.numberOfColumns; x++ )
 		{
 			NSCell	*	theCell = [self preparedCellAtColumn: x row: inRow];
@@ -49,10 +58,40 @@
 }
 
 
+-(NSArray*)	alternatingRowColors
+{
+	static NSArray*	sAltRowColors = nil;
+	if( !sAltRowColors )
+		sAltRowColors = [[NSArray alloc] initWithObjects: NSColor.blueColor, [NSColor colorWithCalibratedRed: 0 green: 0 blue: 0.8 alpha: 1.0], nil];
+	return sAltRowColors;
+}
+
+
 -(void)	drawBackgroundInClipRect: (NSRect)inClipRect
 {
-	[NSColor.redColor set];
-	[NSBezierPath fillRect: inClipRect];
+	NSRect		lastRowBox = self.bounds;
+	if( self.numberOfRows > 0 )
+		lastRowBox = [self rectOfRow: self.numberOfRows -1];
+	else
+	{
+		lastRowBox.origin.y = lastRowBox.size.height;
+		lastRowBox.size.height = self.rowHeight;
+	}
+	
+	NSArray	*	altRowColors = self.alternatingRowColors;
+	NSInteger	currFakeRowIndex = self.numberOfRows;
+	NSRect		currRowBox = lastRowBox;
+	currRowBox.origin.y += lastRowBox.size.height;
+		
+	while( NSMinY(currRowBox) < NSMaxY(inClipRect) )
+	{
+		NSColor*	currColor = [altRowColors objectAtIndex: currFakeRowIndex % altRowColors.count];
+		[currColor set];
+		[NSBezierPath fillRect: currRowBox];
+		
+		currRowBox.origin.y += currRowBox.size.height;
+		currFakeRowIndex++;
+	}
 }
 
 @end
